f83437eacdf17160515d2df5f845fb94f9cc3357
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-06-22 12:17:31 +0530

    feat: Create RouteModel Nodes
    pushed all nodes of Model:Node to m_nodes

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,9 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-
+  int counter = 0;
+  for (Model::Node node : this->Nodes()) {
+      m_Nodes.push_back(Node(counter, this, node));
+      counter++;
+  }
 }
