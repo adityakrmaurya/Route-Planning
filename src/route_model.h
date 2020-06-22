5e79eefb027e6dedea99bceed6b8c8c78d356a87
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-06-22 11:04:23 +0530

    feat: RouteModel variable m_node and public getter function SNodes()
    m_Nodes : Store all of the nodes from the Open Street Map Data.
    SNodes(): a public getter method to return a reference to the vector of
              Node stored as m_Nodes.

--- src/route_model.h
+++ src/route_model.h
@@ -1,32 +1,35 @@
 #pragma once
 
-#include <limits>
-#include <cmath>
-#include <unordered_map>
 #include "model.h"
+#include <cmath>
 #include <iostream>
+#include <limits>
+#include <unordered_map>
 
 class RouteModel : public Model {
 
+public:
+  class Node : public Model::Node {
   public:
-    class Node : public Model::Node {
-      public:
-        // Add public Node variables and methods here.
-        
-        Node(){}
-        Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
-      private:
-        // Add private Node variables and methods here.
-        int index;
-        RouteModel * parent_model = nullptr;
-    };
-    
-    // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
-    std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    // Add public Node variables and methods here.
+
+    Node() {}
+    Node(int idx, RouteModel *search_model, Model::Node node)
+        : Model::Node(node), parent_model(search_model), index(idx) {}
 
   private:
-    // Add private RouteModel variables and methods here.
+    // Add private Node variables and methods here.
+    int index;
+    RouteModel *parent_model = nullptr;
+  };
 
+  // Add public RouteModel variables and methods here.
+  RouteModel(const std::vector<std::byte> &xml);
+  std::vector<Node> path; // This variable will eventually store the path that
+                          // is found by the A* search.
+  auto &SNodes() { return m_Nodes; };
+
+private:
+  // Add private RouteModel variables and methods here.
+  std::vector<Node> m_Nodes;
 };
