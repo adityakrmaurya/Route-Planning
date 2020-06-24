a181071c6a32d6d13e1075c3bed2f0f4a9c8b8de
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-06-24 09:26:28 +0530

    feat: Route Planner header
    Added the following variables:
    start_node and end_node: To store the location of start and end node
    distance: The actual distance between two nodes
    GetDistance(): getter function for distance

--- src/route_planner.h
+++ src/route_planner.h
@@ -1,17 +1,20 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <string>
 #include "route_model.h"
-
+#include <iostream>
+#include <string>
+#include <vector>
 
 class RoutePlanner {
-  public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
+public:
+  RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x,
+               float end_y);
+  // Add public variables or methods declarations here.
+  float GetDistance() { return distance; }
 
-  private:
-    // Add private variables or methods declarations here.
-    RouteModel &m_Model;
+private:
+  // Add private variables or methods declarations here.
+  RouteModel &m_Model;
+  RouteModel::Node *start_node, *end_node;
+  float distance;
 };
