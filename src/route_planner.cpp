3a66d86513b957faf1f153fa6dd6d9446a9f3709
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-06-24 09:41:00 +0530

    feat: The RoutePlanner Constructor
    It converts the start point and end point coordinate into percentage and
    calculate the closest start and closest end node.

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -2,5 +2,11 @@
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-
+    // converting the inputs to percentage
+    start_x *= 0.01;
+    start_y *= 0.01;
+    end_x *= 0.01;
+    end_y *= 0.01;
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
